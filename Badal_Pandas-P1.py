209bf3ec9e79a095c1842de1e4bca7971299a6b5
khalil-badal <khalil.badal.eng@ust.edu.ph>
2024-09-18 16:02:37 +0800

    Update Badal_Pandas-P1.py

--- Badal_Pandas-P1.py
+++ Badal_Pandas-P1.py
@@ -1,6 +1,8 @@
 # EXPERIMENT 3 - PANDAS
+
 # PROBLEM 1: 
 # Using knowledge obtained from the experiment and demonstrations:
+
 # a. Load the corresponding .csv file into a data frame named cars using pandas
 # b. Display the first five and last five rows of the resulting cars.
 
